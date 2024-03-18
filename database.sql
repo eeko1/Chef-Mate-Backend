6bc394ae44252957c4392f91053c402b09a7a1b9
Eemi <eemi.korhonen@metropolia.fi>
2024-03-18 11:56:26 +0200

    database script modified

--- database.sql
+++ database.sql
@@ -2,6 +2,11 @@ DROP DATABASE IF EXISTS ChefMate;
 CREATE DATABASE ChefMate;
 USE ChefMate;
 
+CREATE TABLE UserLevels (
+    level_id INT AUTO_INCREMENT PRIMARY KEY,
+    level_name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -9,4 +14,60 @@ CREATE TABLE Users (
     email VARCHAR(100) NOT NULL UNIQUE,
     user_level_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_level_id) REFERENCES UserLevels(level_id)
+);
+
+CREATE TABLE Recipes (
+    recipes_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    filesize INT NOT NULL,
+    media_type VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    ingredients VARCHAR(255) NOT NULL,
+    description VARCHAT(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    recipes_id INT NOT NULL,
+    user_id INT NOT NULL,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipes_id) REFERENCES Recipes(recipes_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Likes (
+    like_id INT AUTO_INCREMENT PRIMARY KEY,
+    recipes_id INT NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipes_id) REFERENCES Recipes(recipes_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Ratings (
+    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    recipes_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating_value INT NOT NULL CHECK (rating_value BETWEEN 1 AND 5),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipes_id) REFERENCES Recipes(recipes_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Tags (
+    tag_id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE RecipeTags (
+    recipes_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (recipes_id, tag_id),
+    FOREIGN KEY (recipes_id) REFERENCES Recipes(recipes_id),
+    FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
 );
